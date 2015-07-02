4c70462c4f8c3fdf5d7fcc1e65997bce395c4e5e
lavendy <wind_olivier@hotmail.com>
2015-07-02 21:57:13 +0900

    init project

--- app/src/main/java/kr/ac/sch/cglab/plantmonitor/MainActivity.java
+++ app/src/main/java/kr/ac/sch/cglab/plantmonitor/MainActivity.java
@@ -3,6 +3,7 @@ package kr.ac.sch.cglab.plantmonitor;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,7 +14,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("Dfsdf", "dsfjsdk");
     }
 
 
